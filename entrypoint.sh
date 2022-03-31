e5890a34978bddf4c3dea94a46b3c225dce07e0f
Ujjwal <info@gotomfe.com>
2022-03-31 08:45:16 -0400

    verbose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-duck --existing $INPUT_EXISTING --assumeyes -p $INPUT_PASSWORD --upload  $INPUT_PROTOCOL://$INPUT_USERNAME@$INPUT_HOST/public_html$INPUT_TARGET ./$INPUT_SRC
+duck --existing $INPUT_EXISTING --password $INPUT_PASSWORD --verbose --assumeyes --upload  $INPUT_PROTOCOL://$INPUT_USERNAME@$INPUT_HOST/public_html$INPUT_TARGET ./$INPUT_SRC
